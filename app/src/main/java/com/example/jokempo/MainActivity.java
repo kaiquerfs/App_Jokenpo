7012c276d60e9901cc3905bec15772cc49065b8e
Kaique <kaiquerfs53@gmail.com>
2023-09-13 11:03:50 -0300

    Correção layout

--- app/src/main/java/com/example/jokempo/MainActivity.java
+++ app/src/main/java/com/example/jokempo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jokempo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
@@ -10,6 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+    }
+    public void mudarImg(View view){
+        ImageView vc = findViewById(R.id.vc);
+        ImageView pedra = findViewById(R.id.pedra);
+vc.setImageResource(R.drawable.pedra);
     }
 }
 
