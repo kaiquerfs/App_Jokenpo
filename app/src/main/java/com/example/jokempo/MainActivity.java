9dd284862e0058527314e8e7ee0bf6fd311fa2a0
Kaique <kaiquerfs53@gmail.com>
2023-09-14 12:47:31 -0300

    up

--- app/src/main/java/com/example/jokempo/MainActivity.java
+++ app/src/main/java/com/example/jokempo/MainActivity.java
@@ -7,16 +7,38 @@ import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
     }
-    public void mudarImg(View view){
+
+    public int mudarPedra(View view) {
         ImageView vc = findViewById(R.id.vc);
-vc.setImageResource(R.drawable.pedra);
+        vc.setImageResource(R.drawable.pedra);
+        sortear();
+        return 0;
     }
 
+    public int sortear() {
+        Random ran = new Random();
+        int jogada = ran.nextInt(3);
+        ImageView ia = findViewById(R.id.ia);
+        switch (jogada) {
+            case 0:
+                ia.setImageResource(R.drawable.pedra);
+                break;
+            case 1:
+                ia.setImageResource(R.drawable.papel);
+                break;
+            case 2:
+                ia.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return jogada;
+    }
 }
 
